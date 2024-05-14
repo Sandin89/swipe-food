14ffbf7033f3c8185a153bc6cf54da9f1da1746e
Sandin89 <daniel.sandin@outlook.com>
2024-05-14 23:07:51 +0200

    Bakgrundsbilden-jinja-sux

--- swiper/__init__.py
+++ swiper/__init__.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, render_template
-
 from swiper.models import Recipe
 from . import db_conn
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,25 +10,27 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     configure_app(app, test_config)
     db_conn.init_app(app)
-    
+
     if test_config is None:
         app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
-        
+
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-      
+
     @app.route("/")
     def index():
-        return render_template("index.html", static_url_path='/static')
-      
+        recipes = Recipe.query.all()
+        return render_template("index.html", recipes=recipes)
+
     @app.route("/recipe")
     def recipe():
-      recipe = Recipe.query.all()
-      return render_template("recipe.html", recipes=recipe)
+        recipes = Recipe.query.all()
+        return render_template("recipe.html", recipes=recipes)
+
     return app
 
 def configure_app(app, test_config):
@@ -41,14 +41,11 @@ def configure_app(app, test_config):
     )
 
     if test_config is None:
-        app.config.from_pyfile(
-            "config.py", silent=True
-        ) 
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.update(test_config)
+    
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-
